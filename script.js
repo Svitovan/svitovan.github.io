5e187a8663423fa386e2df88803f5796a0da0207
Svitovan <svitovan@outlook.com>
2025-01-03 23:27:54 +0100

    animation

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const todayYear = fullDate.getFullYear();
 
 document.getElementById("year").innerText = todayYear;
 
+// Select all elements with the class 'animate-on-scroll'
+const elements = document.querySelectorAll(".animate-on-scroll");
+// Select the element
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        // Uncomment below if you want the animation to run only once
+        observer.unobserve(entry.target);
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.2, // Trigger when 20% of the element is visible
+  }
+);
+
+// Observe each element
+elements.forEach((element) => {
+  observer.observe(element);
+});
